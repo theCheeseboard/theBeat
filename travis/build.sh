b4740727137dd00859358ea208109cdd13950284
Victor Tran <vicr12345@gmail.com>
2018-11-09 19:25:17 +1100

    Fix Travis

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,6 @@
 set -e
 sudo pacman -S --noconfirm git qt5-base qt5-charts qt5-location qt5-tools phonon-qt5 taglib wget fuse2
+modprobe fuse
 
 git clone https://aur.archlinux.org/the-libs.git
 cd the-libs
