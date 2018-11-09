4a075b74abcc8b3e022aedd9476780ef0f1ac35b
Victor Tran <vicr12345@gmail.com>
2018-11-09 19:59:01 +1100

    Fix Travis

--- travis/build.sh
+++ travis/build.sh
@@ -1,5 +1,5 @@
 set -e
-sudo pacman -S --noconfirm git qt5-base qt5-charts qt5-location qt5-tools phonon-qt5 taglib wget fuse2 fuse3
+sudo pacman -S --noconfirm git qt5-base qt5-charts qt5-location qt5-tools phonon-qt5 taglib wget
 
 git clone https://aur.archlinux.org/the-libs.git
 cd the-libs
@@ -12,3 +12,9 @@ git checkout blueprint
 qmake
 make
 make install INSTALL_ROOT=~/appdir
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+echo "[TRAVIS] Building AppImage"
+./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
+./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
+cp theBeat*.AppImage* ~
--- travis/publish.sh
+++ travis/publish.sh
@@ -1,12 +1,4 @@
-docker cp archbuild:/home/travis/appdir ~/appdir
-ls ~/appdir
-
 cd
-wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-echo "[TRAVIS] Building AppImage"
-./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
-./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
 
 echo "[TRAVIS] Publishing AppImage"
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
