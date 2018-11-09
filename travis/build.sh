e587f8e0e94cd786a62ce181abba749ecdb972ff
Victor Tran <vicr12345@gmail.com>
2018-11-09 18:50:33 +1100

    Fix Travis

--- travis/build.sh
+++ travis/build.sh
@@ -1,4 +1,10 @@
 set -e
+sudo pacman -S --noconfirm git qt5-base qt5-charts qt5-location qt5-tools phonon-qt5
+
+git clone https://aur.archlinux.org/the-libs.git
+cd the-libs
+makepkg -i --noconfirm
+
 git clone https://github.com/vicr123/thebeat.git
 cd thebeat
 git checkout blueprint
