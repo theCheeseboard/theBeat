d65ab183084326c485eb69c7688a7acc56bb0508
Victor Tran <vicr12345@gmail.com>
2018-11-09 19:37:47 +1100

    Fix Travis

--- travis/build.sh
+++ travis/build.sh
@@ -13,9 +13,3 @@ git checkout blueprint
 qmake
 make
 make install INSTALL_ROOT=~/appdir
-wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-echo "[TRAVIS] Building AppImage"
-./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
-./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
-cp theBeat*.AppImage* ~
--- travis/publish.sh
+++ travis/publish.sh
@@ -1,4 +1,10 @@
 cd
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+echo "[TRAVIS] Building AppImage"
+./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -bundle-non-qt-libs -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
+./linuxdeployqt-continuous-x86_64.AppImage ~/appdir/usr/share/applications/*.desktop -appimage -extra-plugins=iconengines/libqsvgicon.so,imageformats/libqsvg.so
+
 echo "[TRAVIS] Publishing AppImage"
 wget -c https://github.com/probonopd/uploadtool/raw/master/upload.sh
 bash upload.sh theBeat*.AppImage*
