c3d4152ed93990261e1406b27716475bcfc7928e
Victor Tran <vicr12345@gmail.com>
2018-11-09 22:56:44 +1100

    Add translations

---
+++ translations/ru_RU.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU" sourcelanguage="en_US">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../aboutwindow.ui" line="14"/>
+        <source>About theBeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="29"/>
+        <source>theBeat 2.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="36"/>
+        <source>Audio Player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="56"/>
+        <source>Copyright (c) Victor Tran 2018. Licensed under the terms of the GNU General Public License, version 3 or later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="81"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LibraryManageWidget</name>
+    <message>
+        <location filename="../librarymanagewidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../librarymanagewidget.ui" line="71"/>
+        <source>Manage Music Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MAC_APPLICATION_MENU</name>
+    <message>
+        <location filename="../main.cpp" line="25"/>
+        <source>Services</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="26"/>
+        <source>Hide %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="27"/>
+        <source>Hide Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="28"/>
+        <source>Show All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="29"/>
+        <source>Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="30"/>
+        <source>About %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="31"/>
+        <source>Quit %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>theBeat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Visualiser</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <location filename="../mainwindow.ui" line="287"/>
+        <location filename="../mainwindow.ui" line="451"/>
+        <source>Music Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="134"/>
+        <location filename="../mainwindow.ui" line="641"/>
+        <source>Playlists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="698"/>
+        <source>Open File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>Open Network Stream</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <location filename="../mainwindow.ui" line="943"/>
+        <source>Play CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="342"/>
+        <source>Search...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="361"/>
+        <location filename="../mainwindow.ui" line="470"/>
+        <location filename="../mainwindow.ui" line="572"/>
+        <source>Manage Music Library</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="534"/>
+        <source>Welcome to theBeat!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="544"/>
+        <source>Let&apos;s get your music library set up so we can find all your music and collate it all into one place.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="738"/>
+        <source>Open an audio file from your computer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="760"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="773"/>
+        <source>Alternatively, drag in music from all around into the playlist box on the right to add it to the playlist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="822"/>
+        <source>Play Network Stream</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="862"/>
+        <source>Enter the URL of the network stream to play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="869"/>
+        <source>Network Stream URL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="891"/>
+        <source>Add</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="983"/>
+        <source>CD Playback coming soon. Stay tuned!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="993"/>
+        <source>Loading CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1404"/>
+        <source>Fi&amp;le</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1411"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1424"/>
+        <source>&amp;Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1427"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1436"/>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1439"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1450"/>
+        <source>Scope</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1458"/>
+        <source>Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1466"/>
+        <source>Circle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1475"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1484"/>
+        <source>Clear Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1493"/>
+        <location filename="../mainwindow.cpp" line="482"/>
+        <source>Save Playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1502"/>
+        <source>Add to existing playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1510"/>
+        <source>Bars</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="88"/>
+        <source>For current playlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="482"/>
+        <source>What do you want to call the playlist?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>Playlist already exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>The playlist called &lt;b&gt;%1&lt;/b&gt; already exists. Do you want to clear it and replace it with the current one?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ translations/vi_VN.ts
@@ -0,0 +1,285 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi_VN" sourcelanguage="en_US">
+<context>
+    <name>AboutWindow</name>
+    <message>
+        <location filename="../aboutwindow.ui" line="14"/>
+        <source>About theBeat</source>
+        <translation>Giới thiệu về theBeat</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="29"/>
+        <source>theBeat 2.0</source>
+        <translation>theBeat 2.0</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="36"/>
+        <source>Audio Player</source>
+        <translation>Bộ nghe nhạc</translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="56"/>
+        <source>Copyright (c) Victor Tran 2018. Licensed under the terms of the GNU General Public License, version 3 or later.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aboutwindow.ui" line="81"/>
+        <source>OK</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LibraryManageWidget</name>
+    <message>
+        <location filename="../librarymanagewidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../librarymanagewidget.ui" line="71"/>
+        <source>Manage Music Library</source>
+        <translation>Quản lý thư viện nhạc</translation>
+    </message>
+</context>
+<context>
+    <name>MAC_APPLICATION_MENU</name>
+    <message>
+        <location filename="../main.cpp" line="25"/>
+        <source>Services</source>
+        <translation>Dịch vụ</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="26"/>
+        <source>Hide %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="27"/>
+        <source>Hide Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="28"/>
+        <source>Show All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="29"/>
+        <source>Preferences...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="30"/>
+        <source>About %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="31"/>
+        <source>Quit %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../mainwindow.ui" line="74"/>
+        <source>theBeat</source>
+        <translation>theBeat</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="116"/>
+        <source>Visualiser</source>
+        <translation>Hình dung</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="125"/>
+        <location filename="../mainwindow.ui" line="287"/>
+        <location filename="../mainwindow.ui" line="451"/>
+        <source>Music Library</source>
+        <translation>Thư viện nhạc</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="134"/>
+        <location filename="../mainwindow.ui" line="641"/>
+        <source>Playlists</source>
+        <translation>Hãng nhạc đã lưu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="143"/>
+        <location filename="../mainwindow.ui" line="698"/>
+        <source>Open File</source>
+        <translation>Mở tệp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="152"/>
+        <source>Open Network Stream</source>
+        <translation>Mở từ mạng</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="161"/>
+        <location filename="../mainwindow.ui" line="943"/>
+        <source>Play CD</source>
+        <translation>Phát đĩa CD</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="342"/>
+        <source>Search...</source>
+        <translation>Tìm kiếm...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="361"/>
+        <location filename="../mainwindow.ui" line="470"/>
+        <location filename="../mainwindow.ui" line="572"/>
+        <source>Manage Music Library</source>
+        <translation>Quản lý thư viện nhạc</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="534"/>
+        <source>Welcome to theBeat!</source>
+        <translation>Chào mừng đến theBeat!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="544"/>
+        <source>Let&apos;s get your music library set up so we can find all your music and collate it all into one place.</source>
+        <translation>Bây giờ bạn sẽ cần đặt thư viện nhạc để chúng tôi tìm kiếm tất cả nhạc của bạn và xếp vào một chỗ.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="738"/>
+        <source>Open an audio file from your computer</source>
+        <translation>Mở tệp nhạc từ máy tính của bạn</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="760"/>
+        <source>Open</source>
+        <translation>Mở</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="773"/>
+        <source>Alternatively, drag in music from all around into the playlist box on the right to add it to the playlist.</source>
+        <translation>Hoặc kéo nhắc đến hàng phát nhạc trên bên phải để bỏ thêm vào hàng nhạc.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="822"/>
+        <source>Play Network Stream</source>
+        <translation>Phát từ mạng</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="862"/>
+        <source>Enter the URL of the network stream to play</source>
+        <translation>Nhập vào URL của luồng dữ liệu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="869"/>
+        <source>Network Stream URL</source>
+        <translation>URL của luồng dữ liệu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="891"/>
+        <source>Add</source>
+        <translation>Thêm</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="983"/>
+        <source>CD Playback coming soon. Stay tuned!</source>
+        <translation>Phát lại CD sắp ra mắt. Hãy theo dõi!</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="993"/>
+        <source>Loading CD</source>
+        <translation>Đang tải đĩa CD</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1404"/>
+        <source>Fi&amp;le</source>
+        <translation>&amp;Tệp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1411"/>
+        <source>Help</source>
+        <translation>Trợ giúp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1424"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Mở</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1427"/>
+        <source>Ctrl+O</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1436"/>
+        <source>&amp;Exit</source>
+        <translation>&amp;Thoát</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1439"/>
+        <source>Ctrl+Q</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1450"/>
+        <source>Scope</source>
+        <translation>Dao động</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1458"/>
+        <source>Lines</source>
+        <translation>Hàng</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1466"/>
+        <source>Circle</source>
+        <translation>Vòng tròn</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1475"/>
+        <source>&amp;About</source>
+        <translation>&amp;Giới thiệu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1484"/>
+        <source>Clear Playlist</source>
+        <translation>Xóa hàng phát nhạc</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1493"/>
+        <location filename="../mainwindow.cpp" line="482"/>
+        <source>Save Playlist</source>
+        <translation>Lưu hàng phát nhạc</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1502"/>
+        <source>Add to existing playlist</source>
+        <translation>Thêm vào hàng nhạc đã lưu</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="1510"/>
+        <source>Bars</source>
+        <translation>Thanh</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="88"/>
+        <source>For current playlist</source>
+        <translation>Cho hàng phát nhạc này</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="482"/>
+        <source>What do you want to call the playlist?</source>
+        <translation>Bạn muốn kêu hàng phát nhạc này là gì?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>Playlist already exists</source>
+        <translation>Hàng phát nhạc này đã tồn tại</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="487"/>
+        <source>The playlist called &lt;b&gt;%1&lt;/b&gt; already exists. Do you want to clear it and replace it with the current one?</source>
+        <translation>Hàng phát nhạc gọi &lt;b&gt;%1&lt;/b&gt; đã tồn tại. Bạn có muốn thay thế nó với hãng phát nhạc này không?</translation>
+    </message>
+</context>
+</TS>
