feded9f046f4fc83354035400cd20a1314c125cc
Victor Tran <vicr12345@gmail.com>
2020-08-12 19:45:40 +1000

    Add translations to cdrdao plugin

--- plugins/CdrdaoPlugin/CdrdaoPlugin.pro
+++ plugins/CdrdaoPlugin/CdrdaoPlugin.pro
@@ -21,6 +21,9 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
+# Include the-libs build tools
+include(/usr/share/the-libs/pri/gentranslations.pri)
+
 SOURCES += \
     burndevice.cpp \
     burnjob.cpp \
---
+++ plugins/CdrdaoPlugin/translations/ar_PS.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ps_AF" sourcelanguage="en_US">
+<context>
+    <name>BurnJob</name>
+    <message>
+        <location filename="../burnjob.cpp" line="57"/>
+        <location filename="../burnjob.cpp" line="112"/>
+        <source>Preparing to burn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="86"/>
+        <source>Preparing Track %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="98"/>
+        <source>Couldn&apos;t transcode track</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="174"/>
+        <source>Burning Track %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="178"/>
+        <source>Finalising CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="209"/>
+        <source>Cancelled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="211"/>
+        <source>Couldn&apos;t burn tracks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjob.cpp" line="238"/>
+        <source>Burn Successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BurnJobWidget</name>
+    <message>
+        <location filename="../burnjobwidget.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjobwidget.ui" line="26"/>
+        <source>BURN CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnjobwidget.ui" line="52"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BurnPopover</name>
+    <message>
+        <location filename="../burnpopover.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.ui" line="51"/>
+        <source>BURN OPTIONS</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.ui" line="58"/>
+        <source>Album Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.ui" line="65"/>
+        <source>Burn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.ui" line="97"/>
+        <source>HEADS UP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="45"/>
+        <location filename="../burnpopover.cpp" line="82"/>
+        <source>Burn %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="94"/>
+        <source>Insert a CD-R or a CD-RW into the drive.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="98"/>
+        <source>The CD in the drive is not blank. Erase the CD first.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="102"/>
+        <source>The CD-R in the drive has already been written.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="130"/>
+        <source>This playlist is too long to fit on the CD.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burnpopover.cpp" line="134"/>
+        <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
