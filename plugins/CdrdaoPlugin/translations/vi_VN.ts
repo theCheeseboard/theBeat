39e7ba118198b58d14e7d8f1b736d94dfbfad471
Victor Tran <vicr12345@gmail.com>
2020-10-01 10:31:45 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 95.6% (22 of 23 strings)
    
    Translation: theBeat/theBeat cdrdao Integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-cdrdao-integration/vi/

--- plugins/CdrdaoPlugin/translations/vi_VN.ts
+++ plugins/CdrdaoPlugin/translations/vi_VN.ts
@@ -1,64 +1,64 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>BurnJob</name>
     <message>
         <location filename="../burnjob.cpp" line="65"/>
         <location filename="../burnjob.cpp" line="113"/>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị ghi đĩa</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="94"/>
         <source>Preparing Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị bài %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="103"/>
         <source>Couldn&apos;t transcode track</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể chuyển mã bài hát</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="175"/>
         <source>Burning Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang ghi bài %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="181"/>
         <source>Finalising CD</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang hoàn thiện CD</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="225"/>
         <source>Cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Bị hủy</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="227"/>
         <source>Couldn&apos;t burn tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghi bài</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="253"/>
         <location filename="../burnjob.cpp" line="259"/>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa thành công</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="259"/>
         <source>Burned &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Được ghi &quot;%1&quot; vào đĩa</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="274"/>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa không thành công</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="274"/>
         <source>Failed to burn &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghi &quot;%1&quot; vào đĩa</translation>
     </message>
 </context>
 <context>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../burnjobwidget.ui" line="26"/>
         <source>BURN CD</source>
-        <translation type="unfinished"></translation>
+        <translation>GHI ĐĨA</translation>
     </message>
     <message>
         <location filename="../burnjobwidget.ui" line="55"/>
@@ -79,53 +79,53 @@
     <message>
         <location filename="../burnpopover.ui" line="51"/>
         <source>BURN OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>TÙY CHỌN GHI ĐĨA</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="58"/>
         <source>Album Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên đĩa nhạc</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="65"/>
         <source>Burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="97"/>
         <source>HEADS UP</source>
-        <translation type="unfinished"></translation>
+        <translation>CHÚ Ý</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="45"/>
         <location filename="../burnpopover.cpp" line="82"/>
         <source>Burn %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi %1</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="94"/>
         <source>Insert a CD-R or a CD-RW into the drive.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hãy chèn một đĩa CD-R hoặc CD-RW vào.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="98"/>
         <source>The CD in the drive is not blank. Erase the CD first.</source>
-        <translation type="unfinished"></translation>
+        <translation>Đĩa CD trong ổ đĩa không có trống. Xóa đĩa CD trước.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="102"/>
         <source>The CD-R in the drive has already been written.</source>
-        <translation type="unfinished"></translation>
+        <translation>Đĩa CD-R trong ổ đĩa đã bị ghi vào.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="130"/>
         <source>This playlist is too long to fit on the CD.</source>
-        <translation type="unfinished"></translation>
+        <translation>Danh sách này quá dài để vừa trên CD.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="134"/>
         <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
-        <translation type="unfinished"></translation>
+        <translation>Đĩa CD trong ổ đĩa có thể bị ghi lại, vì vạy đĩa này có thể không hoạt động trong bộ phát CD cũ.</translation>
     </message>
 </context>
 </TS>
