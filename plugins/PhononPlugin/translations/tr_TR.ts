eca587a2b9f7bca360e13c90163d78d457432696
Atahan Reis <atahan.r@outlook.com>
2020-10-22 14:57:27 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (25 of 25 strings)
    
    Translation: theBeat/theBeat Phonon Integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-phonon-integration/tr/

--- plugins/PhononPlugin/translations/tr_TR.ts
+++ plugins/PhononPlugin/translations/tr_TR.ts
@@ -5,115 +5,115 @@
     <name>CdChecker</name>
     <message>
         <source>CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <source>Enqueue All</source>
-        <translation type="unfinished"></translation>
+        <translation>Tümünü Sırala</translation>
     </message>
     <message>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Çıkar</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>İthalat</translation>
     </message>
 </context>
 <context>
     <name>ImportCdJob</name>
     <message>
         <source>Preparing to import</source>
-        <translation type="unfinished"></translation>
+        <translation>İçe aktarmaya hazırlanıyor</translation>
     </message>
     <message>
         <source>Couldn&apos;t query disc info</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk bilgisi sorgulanamadı</translation>
     </message>
     <message>
         <source>Importing %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 içe aktarılıyor</translation>
     </message>
     <message>
         <source>Import Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Başarılı İçe Aktarma</translation>
     </message>
     <message>
         <source>Couldn&apos;t import track</source>
-        <translation type="unfinished"></translation>
+        <translation>Parça içe aktarılamadı</translation>
     </message>
     <message>
         <source>Cancelling</source>
-        <translation type="unfinished"></translation>
+        <translation>Vazgeçiyor</translation>
     </message>
     <message>
         <source>Cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Vazgeçti</translation>
     </message>
     <message>
         <source>CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <source>Imported &quot;%1&quot; successfully</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; başarıyla içe aktarıldı</translation>
     </message>
     <message>
         <source>Import Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>İçe Aktarma Hatası</translation>
     </message>
     <message>
         <source>Failed to import &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; alınamadı</translation>
     </message>
 </context>
 <context>
     <name>ImportCdJobWidget</name>
     <message>
         <source>IMPORT CD</source>
-        <translation type="unfinished"></translation>
+        <translation>İTHALAT CD&apos;Sİ</translation>
     </message>
 </context>
 <context>
     <name>ImportCdPopover</name>
     <message>
         <source>Folder</source>
-        <translation type="unfinished"></translation>
+        <translation>Klasör</translation>
     </message>
     <message>
         <source>Browse...</source>
-        <translation type="unfinished"></translation>
+        <translation>Araştır...</translation>
     </message>
     <message>
         <source>IMPORT OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>İTHALAT SEÇENEKLERİ</translation>
     </message>
     <message>
         <source>Import %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1&apos;i içe aktar</translation>
     </message>
     <message>
         <source>Ogg Vorbis</source>
-        <translation type="unfinished"></translation>
+        <translation>Ogg Vorbis</translation>
     </message>
     <message>
         <source>Format</source>
-        <translation type="unfinished"></translation>
+        <translation>Biçim</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>İthalat</translation>
     </message>
 </context>
 <context>
     <name>TrackInfo</name>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilinmeyen</translation>
     </message>
     <message>
         <source>Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>1% Parça</translation>
     </message>
 </context>
 </TS>
