39f9b6c5ce0e7f9fe53cb5c4f2255e4a2a11d0dc
Atahan Reis <atahan.r@outlook.com>
2020-10-22 02:04:05 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: theBeat/theBeat cdrdao Integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-cdrdao-integration/tr/

--- plugins/CdrdaoPlugin/translations/tr_TR.ts
+++ plugins/CdrdaoPlugin/translations/tr_TR.ts
@@ -7,58 +7,58 @@
         <location filename="../burnjob.cpp" line="65"/>
         <location filename="../burnjob.cpp" line="113"/>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanmaya hazırlanıyor</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="94"/>
         <source>Preparing Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Parçası hazırlanıyor</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="103"/>
         <source>Couldn&apos;t transcode track</source>
-        <translation type="unfinished"></translation>
+        <translation>Parçanın kodu dönüştürülemedi</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="175"/>
         <source>Burning Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazan Parça %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="181"/>
         <source>Finalising CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD tamamlanıyor</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="225"/>
         <source>Cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>İptal edildi</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="227"/>
         <source>Couldn&apos;t burn tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>İzler yazılamadı</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="253"/>
         <location filename="../burnjob.cpp" line="259"/>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Başarılı Yandı</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="259"/>
         <source>Burned &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; diske yazdırıldı</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="274"/>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Başarısız Yanık</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="274"/>
         <source>Failed to burn &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; diske yazılamadı</translation>
     </message>
 </context>
 <context>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../burnjobwidget.ui" line="26"/>
         <source>BURN CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD OLUŞTURMAK</translation>
     </message>
 </context>
 <context>
@@ -74,53 +74,53 @@
     <message>
         <location filename="../burnpopover.ui" line="51"/>
         <source>BURN OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>YAKMA SEÇENEKLERİ</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="58"/>
         <source>Album Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Albüm Adı</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="65"/>
         <source>Burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanmak</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="97"/>
         <source>HEADS UP</source>
-        <translation type="unfinished"></translation>
+        <translation>DİKKAT ET</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="45"/>
         <location filename="../burnpopover.cpp" line="82"/>
         <source>Burn %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 Yak</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="94"/>
         <source>Insert a CD-R or a CD-RW into the drive.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sürücüye bir CD-R veya CD-RW yerleştirin.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="98"/>
         <source>The CD in the drive is not blank. Erase the CD first.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sürücüdeki CD boş değil. Önce CD&apos;yi silin.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="102"/>
         <source>The CD-R in the drive has already been written.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sürücüdeki CD-R zaten yazılmıştır.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="130"/>
         <source>This playlist is too long to fit on the CD.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu çalma listesi CD&apos;ye sığmayacak kadar uzun.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="134"/>
         <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sürücüdeki CD yeniden yazılabilir, bu nedenle yazılan CD daha eski CD oynatıcılarda çalışmayabilir.</translation>
     </message>
 </context>
 </TS>
