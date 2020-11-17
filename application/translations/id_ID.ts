72ae9c3191af6220b580e501af1d3a0b3f7addd4
Zumi <daxuya.zumi+code@protonmail.com>
2020-11-17 14:26:37 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 51.2% (60 of 117 strings)
    
    Translation: theBeat/theBeat 3.0
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-3-0/id/

--- application/translations/id_ID.ts
+++ application/translations/id_ID.ts
@@ -7,22 +7,22 @@
         <location filename="../artistsalbumswidget.ui" line="65"/>
         <location filename="../artistsalbumswidget.ui" line="188"/>
         <source>Tracks in Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek dalam Pustaka</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.ui" line="230"/>
         <source>Enqueue All</source>
-        <translation type="unfinished"></translation>
+        <translation>Antri Semua</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.ui" line="241"/>
         <source>Play All</source>
-        <translation type="unfinished"></translation>
+        <translation>Putar Semua</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.ui" line="252"/>
         <source>Shufle All</source>
-        <translation type="unfinished"></translation>
+        <translation>Acak Semua</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.ui" line="276"/>
@@ -32,28 +32,28 @@
     <message>
         <location filename="../artistsalbumswidget.cpp" line="67"/>
         <source>Albums in Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Album dalam Pustaka</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.cpp" line="67"/>
         <source>Artists in Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Artis dalam Pustaka</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.cpp" line="153"/>
         <source>Tracks in %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek dalam %1</translation>
     </message>
     <message>
         <location filename="../artistsalbumswidget.cpp" line="153"/>
         <source>Tracks by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek dari %1</translation>
     </message>
     <message numerus="yes">
         <location filename="../artistsalbumswidget.cpp" line="162"/>
         <source>%n tracks</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n trek</numerusform>
         </translation>
     </message>
 </context>
@@ -62,7 +62,7 @@
     <message>
         <location filename="../common.cpp" line="31"/>
         <source>Select Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilih Devais</translation>
     </message>
 </context>
 <context>
@@ -70,7 +70,7 @@
     <message>
         <location filename="../controlstrip.ui" line="102"/>
         <source>Title</source>
-        <translation type="unfinished"></translation>
+        <translation>Judul</translation>
     </message>
     <message>
         <location filename="../controlstrip.ui" line="109"/>
@@ -80,12 +80,12 @@
     <message>
         <location filename="../controlstrip.cpp" line="79"/>
         <source>Repeat Options</source>
-        <translation type="unfinished"></translation>
+        <translation>Opsi Pengulangan</translation>
     </message>
     <message>
         <location filename="../controlstrip.cpp" line="80"/>
         <source>Repeat Play Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Ulangi Antrian Pemutaran</translation>
     </message>
 </context>
 <context>
@@ -98,25 +98,25 @@
     <message>
         <location filename="../currenttrackpopover.cpp" line="100"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nama</translation>
     </message>
     <message>
         <location filename="../currenttrackpopover.cpp" line="105"/>
         <location filename="../currenttrackpopover.cpp" line="107"/>
         <location filename="../currenttrackpopover.cpp" line="110"/>
         <source>Track</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek</translation>
     </message>
     <message>
         <location filename="../currenttrackpopover.cpp" line="105"/>
         <source>%1 of %2</source>
         <comment>Track 1 of 12</comment>
-        <translation type="unfinished"></translation>
+        <translation>%1 dari %2</translation>
     </message>
     <message>
         <location filename="../currenttrackpopover.cpp" line="111"/>
         <source>Year</source>
-        <translation type="unfinished"></translation>
+        <translation>Tahun</translation>
     </message>
 </context>
 <context>
@@ -124,13 +124,13 @@
     <message>
         <location filename="../library/libraryenumeratedirectoryjob.cpp" line="153"/>
         <source>Folder Added</source>
-        <translation type="unfinished"></translation>
+        <translation>Folder Ditambahkan</translation>
     </message>
     <message numerus="yes">
         <location filename="../library/libraryenumeratedirectoryjob.cpp" line="154"/>
         <source>%n tracks added/updated</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>%n trek ditambah/diperbarui</numerusform>
         </translation>
     </message>
 </context>
@@ -139,7 +139,7 @@
     <message>
         <location filename="../library/libraryenumeratedirectoryjobwidget.ui" line="26"/>
         <source>DISCOVERING AUDIO TRACKS</source>
-        <translation type="unfinished"></translation>
+        <translation>MENCARI TREK AUDIO</translation>
     </message>
 </context>
 <context>
@@ -152,22 +152,22 @@
     <message>
         <location filename="../libraryerrorpopover.ui" line="134"/>
         <source>ACTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>AKSI</translation>
     </message>
     <message>
         <location filename="../libraryerrorpopover.ui" line="143"/>
         <source>Locate File</source>
-        <translation type="unfinished"></translation>
+        <translation>Cari File</translation>
     </message>
     <message>
         <location filename="../libraryerrorpopover.ui" line="150"/>
         <source>Remove from Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Hapus dari Pustaka</translation>
     </message>
     <message>
         <location filename="../libraryerrorpopover.cpp" line="39"/>
         <source>Looks like the file has gone into hiding. If you know where it is, let us know so we can play the track.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sepertinya file-nya hilang. Jika Anda tahu file-nya berada di mana, silahkan ditambahkan ulang untuk dimainkan.</translation>
     </message>
 </context>
 <context>
@@ -175,17 +175,17 @@
     <message>
         <location filename="../library/librarymodel.cpp" line="174"/>
         <source>by %1</source>
-        <translation type="unfinished"></translation>
+        <translation>oleh %1</translation>
     </message>
     <message>
         <location filename="../library/librarymodel.cpp" line="175"/>
         <source>on %1</source>
-        <translation type="unfinished"></translation>
+        <translation>pada %1</translation>
     </message>
     <message>
         <location filename="../library/librarymodel.cpp" line="177"/>
         <source>Track</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek</translation>
     </message>
 </context>
 <context>
@@ -193,46 +193,46 @@
     <message>
         <location filename="../library/librarylistview.cpp" line="28"/>
         <source>Add to Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Tambahkan ke Playlist</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="43"/>
         <location filename="../library/librarylistview.cpp" line="45"/>
         <source>Remove from Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Hapus dari Pustaka</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="33"/>
         <location filename="../library/librarylistview.cpp" line="44"/>
         <source>Are you sure?</source>
-        <translation type="unfinished"></translation>
+        <translation>Yakin?</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="32"/>
         <location filename="../library/librarylistview.cpp" line="34"/>
         <source>Remove from Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Hapus dari Playlist</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="110"/>
         <source>New Playlist</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlist Baru</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="112"/>
         <source>Playlist Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nama Playlist</translation>
     </message>
     <message>
         <location filename="../library/librarylistview.cpp" line="127"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk %1</translation>
     </message>
     <message numerus="yes">
         <location filename="../library/librarylistview.cpp" line="132"/>
         <source>For %n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Untuk %n item</numerusform>
         </translation>
     </message>
 </context>
@@ -246,57 +246,57 @@
     <message>
         <location filename="../mainwindow.ui" line="90"/>
         <source>Tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Trek</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="116"/>
         <source>Artists</source>
-        <translation type="unfinished"></translation>
+        <translation>Artis</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="139"/>
         <source>Albums</source>
-        <translation type="unfinished"></translation>
+        <translation>Album</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="162"/>
         <source>Playlists</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlist</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="185"/>
         <source>Other Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Sumber Lain</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="276"/>
         <source>Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Antrian</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="346"/>
         <source>Nothing Here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Tidak Ada!</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="403"/>
         <source>File Bug</source>
-        <translation type="unfinished"></translation>
+        <translation>Laporkan Bug</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="412"/>
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Kode Sumber</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="421"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Tentang</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="430"/>
         <source>Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Keluar</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="433"/>
@@ -306,7 +306,7 @@
     <message>
         <location filename="../mainwindow.ui" line="442"/>
         <source>Open File</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka File</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="445"/>
@@ -317,13 +317,13 @@
         <location filename="../mainwindow.ui" line="450"/>
         <location filename="../mainwindow.cpp" line="403"/>
         <source>Open URL</source>
-        <translation type="unfinished"></translation>
+        <translation>Buka URL</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="459"/>
         <location filename="../mainwindow.cpp" line="191"/>
         <source>Skip Back</source>
-        <translation type="unfinished"></translation>
+        <translation>Lompat Mundur</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="462"/>
@@ -338,12 +338,12 @@
     <message>
         <location filename="../mainwindow.ui" line="495"/>
         <source>Add to Library</source>
-        <translation type="unfinished"></translation>
+        <translation>Tambahkan ke Pustaka</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="504"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Pengaturan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="507"/>
@@ -353,7 +353,7 @@
     <message>
         <location filename="../mainwindow.ui" line="515"/>
         <source>theBeat Help</source>
-        <translation type="unfinished"></translation>
+        <translation>Bantuan theBeat</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="518"/>
@@ -363,19 +363,19 @@
     <message>
         <location filename="../mainwindow.ui" line="471"/>
         <source>Skip Forward</source>
-        <translation type="unfinished"></translation>
+        <translation>Lompat Maju</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="356"/>
         <source>Select a track or drop something here!</source>
-        <translation type="unfinished"></translation>
+        <translation>Pilih trek atau drop file di sini!</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="483"/>
         <location filename="../mainwindow.cpp" line="199"/>
         <location filename="../mainwindow.cpp" line="213"/>
         <source>Play</source>
-        <translation type="unfinished"></translation>
+        <translation>Mainkan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="486"/>
@@ -390,29 +390,29 @@
     <message>
         <location filename="../mainwindow.cpp" line="220"/>
         <source>Skip Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Lewati</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="375"/>
         <source>For &quot;%1&quot;</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk &quot;%n&quot;</translation>
     </message>
     <message numerus="yes">
         <location filename="../mainwindow.cpp" line="377"/>
         <source>For %n items</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
+        <translation>
+            <numerusform>Untuk %n item</numerusform>
         </translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="379"/>
         <source>Remove from Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Hapus dari Antrian</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="392"/>
         <source>For Queue</source>
-        <translation type="unfinished"></translation>
+        <translation>Untuk Antrian</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="393"/>
