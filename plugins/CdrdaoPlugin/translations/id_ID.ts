3107425891f04a6eccba50ccbabbe4fb9c733290
Zumi <daxuya.zumi+code@protonmail.com>
2020-11-17 15:22:51 +0000

    Translated using Weblate (Indonesian)
    Currently translated at 86.3% (19 of 22 strings)
    
    Translation: theBeat/theBeat cdrdao Integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-cdrdao-integration/id/

--- plugins/CdrdaoPlugin/translations/id_ID.ts
+++ plugins/CdrdaoPlugin/translations/id_ID.ts
@@ -7,58 +7,58 @@
         <location filename="../burnjob.cpp" line="69"/>
         <location filename="../burnjob.cpp" line="119"/>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Menyiapkan burning</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="97"/>
         <source>Preparing Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Menyiapkan Trek %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="106"/>
         <source>Couldn&apos;t transcode track</source>
-        <translation type="unfinished"></translation>
+        <translation>Tidak dapat transcoding trek</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="183"/>
         <source>Burning Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mem-burn Trek %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="189"/>
         <source>Finalising CD</source>
-        <translation type="unfinished"></translation>
+        <translation>Menyelesaikan CD</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="228"/>
         <source>Cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Dibatalkan</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="230"/>
         <source>Couldn&apos;t burn tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Tidak dapat mem-burn trek</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="257"/>
         <location filename="../burnjob.cpp" line="265"/>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Burning Berhasil</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="265"/>
         <source>Burned &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;%1&quot; telah diburn ke CD</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="280"/>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal Burning</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="280"/>
         <source>Failed to burn &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Gagal mem-burn &quot;%1&quot; ke CD</translation>
     </message>
 </context>
 <context>
@@ -74,12 +74,12 @@
     <message>
         <location filename="../burnpopover.ui" line="51"/>
         <source>BURN OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>PENGATURAN BURN</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="58"/>
         <source>Album Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Nama Album</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="65"/>
@@ -89,7 +89,7 @@
     <message>
         <location filename="../burnpopover.ui" line="97"/>
         <source>HEADS UP</source>
-        <translation type="unfinished"></translation>
+        <translation>PERHATIAN</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="45"/>
@@ -100,27 +100,27 @@
     <message>
         <location filename="../burnpopover.cpp" line="94"/>
         <source>Insert a CD-R or a CD-RW into the drive.</source>
-        <translation type="unfinished"></translation>
+        <translation>Masukkan CD-R atau CD-RW ke dalam drive.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="98"/>
         <source>The CD in the drive is not blank. Erase the CD first.</source>
-        <translation type="unfinished"></translation>
+        <translation>CD di dalam drive belum kosong, silahkan dikosongkan terlebih dahulu.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="102"/>
         <source>The CD-R in the drive has already been written.</source>
-        <translation type="unfinished"></translation>
+        <translation>CD-R pada drive sudah ditulis.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="130"/>
         <source>This playlist is too long to fit on the CD.</source>
-        <translation type="unfinished"></translation>
+        <translation>Playlist terlalu panjang untuk muat ke dalam CD.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="134"/>
         <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
-        <translation type="unfinished"></translation>
+        <translation>CD dalam drive bersifat tulis-ulang, jadi CD yang telah di-burn mungkin tidak dapat dimainkan dalam pemutar CD yang lawas.</translation>
     </message>
 </context>
 </TS>
