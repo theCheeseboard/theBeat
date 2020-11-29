d098fc3bfa3f44685daec525150dea61e9e894a2
Atahan Reis <atahan.r@outlook.com>
2020-11-29 01:09:33 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (117 of 117 strings)
    
    Translation: theBeat/theBeat Desktop
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-3-0/tr/

--- application/translations/tr_TR.ts
+++ application/translations/tr_TR.ts
@@ -54,6 +54,7 @@
         <source>%n tracks</source>
         <translation>
             <numerusform>%n şarkı</numerusform>
+            <numerusform>%n şarkılar</numerusform>
         </translation>
     </message>
 </context>
@@ -130,7 +131,8 @@
         <location filename="../library/libraryenumeratedirectoryjob.cpp" line="154"/>
         <source>%n tracks added/updated</source>
         <translation>
-            <numerusform>%n şarkı eklendi / güncellendi</numerusform>
+            <numerusform>%n şarkı eklendi/güncellendi</numerusform>
+            <numerusform>%n şarkılar eklendi/güncellendi</numerusform>
         </translation>
     </message>
 </context>
@@ -233,6 +235,7 @@
         <source>For %n items</source>
         <translation>
             <numerusform>%n öğe için</numerusform>
+            <numerusform>%n öğeler için</numerusform>
         </translation>
     </message>
 </context>
@@ -402,6 +405,7 @@
         <source>For %n items</source>
         <translation>
             <numerusform>%n öğe için</numerusform>
+            <numerusform>%n öğeleri için</numerusform>
         </translation>
     </message>
     <message>
@@ -647,6 +651,7 @@ Sıfırlama tamamlandığında theBeat yeniden başlayacaktır. Bu eylem geri al
         <source>For %n playlists</source>
         <translation>
             <numerusform>%n oynatma listesi için</numerusform>
+            <numerusform>%n oynatma listesileri için</numerusform>
         </translation>
     </message>
 </context>
