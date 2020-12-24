cf3e703eee882f0cb8a87ea40e0ca4e17857c1d6
Atahan Reis <atahanreis@icloud.com>
2020-12-24 22:26:48 +0000

    Translated using Weblate (English (United Kingdom))
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: theBeat/theBeat cdrdao Integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-cdrdao-integration/en_GB/

--- plugins/CdrdaoPlugin/translations/en_GB.ts
+++ plugins/CdrdaoPlugin/translations/en_GB.ts
@@ -1,64 +1,64 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="en_GB">
 <context>
     <name>BurnJob</name>
     <message>
         <location filename="../burnjob.cpp" line="69"/>
         <location filename="../burnjob.cpp" line="119"/>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparing to burn</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="97"/>
         <source>Preparing Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Preparing Track %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="106"/>
         <source>Couldn&apos;t transcode track</source>
-        <translation type="unfinished"></translation>
+        <translation>Couldn&apos;t transcode track</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="183"/>
         <source>Burning Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Burning Track %1</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="189"/>
         <source>Finalising CD</source>
-        <translation type="unfinished"></translation>
+        <translation>Finalising CD</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="228"/>
         <source>Cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancelled</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="230"/>
         <source>Couldn&apos;t burn tracks</source>
-        <translation type="unfinished"></translation>
+        <translation>Couldn&apos;t burn tracks</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="257"/>
         <location filename="../burnjob.cpp" line="265"/>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Burn Successful</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="265"/>
         <source>Burned %1 to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Burned %1 to disc</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="280"/>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Burn Failure</translation>
     </message>
     <message>
         <location filename="../burnjob.cpp" line="280"/>
         <source>Failed to burn &quot;%1&quot; to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to burn &quot;%1&quot; to disc</translation>
     </message>
 </context>
 <context>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../burnjobwidget.ui" line="26"/>
         <source>BURN CD</source>
-        <translation type="unfinished"></translation>
+        <translation>BURN CD</translation>
     </message>
 </context>
 <context>
@@ -74,53 +74,53 @@
     <message>
         <location filename="../burnpopover.ui" line="51"/>
         <source>BURN OPTIONS</source>
-        <translation type="unfinished"></translation>
+        <translation>BURN OPTIONS</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="58"/>
         <source>Album Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Album Name</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="65"/>
         <source>Burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Burn</translation>
     </message>
     <message>
         <location filename="../burnpopover.ui" line="97"/>
         <source>HEADS UP</source>
-        <translation type="unfinished"></translation>
+        <translation>HEADS UP</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="45"/>
         <location filename="../burnpopover.cpp" line="82"/>
         <source>Burn %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Burn %1</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="94"/>
         <source>Insert a CD-R or a CD-RW into the drive.</source>
-        <translation type="unfinished"></translation>
+        <translation>Insert a CD-R or a CD-RW into the drive.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="98"/>
         <source>The CD in the drive is not blank. Erase the CD first.</source>
-        <translation type="unfinished"></translation>
+        <translation>The CD in the drive is not blank. Erase the CD first.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="102"/>
         <source>The CD-R in the drive has already been written.</source>
-        <translation type="unfinished"></translation>
+        <translation>The CD-R in the drive has already been written.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="130"/>
         <source>This playlist is too long to fit on the CD.</source>
-        <translation type="unfinished"></translation>
+        <translation>This playlist is too long to fit on the CD.</translation>
     </message>
     <message>
         <location filename="../burnpopover.cpp" line="134"/>
         <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
-        <translation type="unfinished"></translation>
+        <translation>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</translation>
     </message>
 </context>
 </TS>
