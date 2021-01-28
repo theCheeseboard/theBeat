d2327f1a02f385e7307d85b44e5614c5a5f1f537
Victor Tran <vicr12345@gmail.com>
2021-01-29 01:55:51 +1100

    Fix argument order

--- ci/extra-steps-mac.sh
+++ ci/extra-steps-mac.sh
@@ -1,3 +1,4 @@
 # Modify the AVFoundation plugin
-install_name_tool -change build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib
-install_name_tool -change build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib
+install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib 
+install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib
+
