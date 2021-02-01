7c248cdf3fbaeffee9ae04c77e72db75bcfc1eed
Victor Tran <vicr12345@gmail.com>
2021-02-01 14:57:27 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 97.0% (33 of 34 strings)
    
    Translation: theBeat/theBeat Mac integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-mac-integration/vi/

--- plugins/MacIntegration/translations/vi_VN.ts
+++ plugins/MacIntegration/translations/vi_VN.ts
@@ -1,113 +1,113 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>CdChecker</name>
     <message>
         <source>CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <source>Enqueue All</source>
-        <translation type="unfinished"></translation>
+        <translation>Đặt tất cả vào hàng phát</translation>
     </message>
     <message>
         <source>Play All</source>
-        <translation type="unfinished"></translation>
+        <translation>Phát tất cả</translation>
     </message>
     <message>
         <source>Shuffle All</source>
-        <translation type="unfinished"></translation>
+        <translation>Phát ngẫu nhiên</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhập</translation>
     </message>
     <message>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Nhổ đĩa</translation>
     </message>
     <message>
         <source>Accessing MusicBrainz...</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang truy cập vào MusicBrainz...</translation>
     </message>
     <message>
         <source>CD NOT FOUND</source>
-        <translation type="unfinished"></translation>
+        <translation>KHÔNG TÌM THẤY ĐĨA CD</translation>
     </message>
     <message>
         <source>This CD was not found on the MusicBrainz CD Database.</source>
-        <translation type="unfinished"></translation>
+        <translation>Đĩa CD này không thể tìm được trong cơ sở dữ liệu MusicBrainz.</translation>
     </message>
     <message>
         <source>SELECT CORRECT ALBUM</source>
-        <translation type="unfinished"></translation>
+        <translation>CHỌN ALBUM ĐÚNG</translation>
     </message>
     <message>
         <source>More than one result was found on MusicBrainz for this CD. Select the correct CD for accurate track information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Được tìm hơn một kết quả trên MusicBrainz cho dĩa CD này. Chọn CD đúng để có thông tin chính xác về bản nhạc.</translation>
     </message>
     <message>
         <source>Couldn&apos;t eject the disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể nhổ đĩa ra</translation>
     </message>
     <message>
         <source>Make sure no other applications are accessing the disc, and then try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Đảm bảo rằng không có ứng dụng nào khác đang sử dụng đĩa, ròi thử lại.</translation>
     </message>
 </context>
 <context>
     <name>MacBurnJob</name>
     <message>
         <source>Burn Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa thành công</translation>
     </message>
     <message>
         <source>Burn cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Đã hủy ghi đĩa</translation>
     </message>
     <message>
         <source>Failed to burn: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghi đĩa: %1</translation>
     </message>
     <message>
         <source>Erasing disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang xóa đĩa</translation>
     </message>
     <message>
         <source>Finalising disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang hoàn thiện đĩa</translation>
     </message>
     <message>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang chuẩn bị ghi đĩa</translation>
     </message>
     <message>
         <source>Burning track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang ghi rãnh %1</translation>
     </message>
     <message>
         <source>Burning</source>
-        <translation type="unfinished"></translation>
+        <translation>Đang ghi</translation>
     </message>
 </context>
 <context>
     <name>MacBurnJobPrivate</name>
     <message>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa thành công</translation>
     </message>
     <message>
         <source>Burned %1 to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Được ghi %1 vào đĩa</translation>
     </message>
     <message>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa không thành công</translation>
     </message>
     <message>
         <source>Failed to burn %1 to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Không thể ghi %1 vào đĩa</translation>
     </message>
 </context>
 <context>
@@ -118,44 +118,44 @@
     </message>
     <message>
         <source>BURN CD</source>
-        <translation type="unfinished"></translation>
+        <translation>GHI ĐĨA</translation>
     </message>
 </context>
 <context>
     <name>MacBurnProvider</name>
     <message>
         <source>Insert a CD-R or CD-RW</source>
-        <translation type="unfinished"></translation>
+        <translation>Chèn một đĩa CD-R hoặc CD-RW</translation>
     </message>
     <message>
         <source>Burn on macOS</source>
-        <translation type="unfinished"></translation>
+        <translation>Ghi đĩa trên macOS</translation>
     </message>
 </context>
 <context>
     <name>MusicBrainzReleaseModel</name>
     <message>
         <source>Released: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Phát hành: %1</translation>
     </message>
     <message>
         <source>Barcode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Mã vạch: %1</translation>
     </message>
     <message>
         <source>Country: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Quốc gia: %1</translation>
     </message>
 </context>
 <context>
     <name>TrackInfo</name>
     <message>
         <source>Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Rãnh %1</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Không xác định</translation>
     </message>
 </context>
 </TS>
