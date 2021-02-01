4198f7d4f41d3106b895f5ba091caa0b500d9ea2
Aleyna Tilki <atahanreis@icloud.com>
2021-02-01 22:59:38 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: theBeat/theBeat Mac integration
    Translate-URL: https://translate.vicr123.com/projects/thebeat/thebeat-mac-integration/tr/

--- plugins/MacIntegration/translations/tr_TR.ts
+++ plugins/MacIntegration/translations/tr_TR.ts
@@ -5,153 +5,153 @@
     <name>CdChecker</name>
     <message>
         <source>CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD</translation>
     </message>
     <message>
         <source>Enqueue All</source>
-        <translation type="unfinished"></translation>
+        <translation>Tümünü Sırala</translation>
     </message>
     <message>
         <source>Play All</source>
-        <translation type="unfinished"></translation>
+        <translation>Hepsini Oynat</translation>
     </message>
     <message>
         <source>Shuffle All</source>
-        <translation type="unfinished"></translation>
+        <translation>Tümünü Karıştır</translation>
     </message>
     <message>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>İthalat</translation>
     </message>
     <message>
         <source>Eject</source>
-        <translation type="unfinished"></translation>
+        <translation>Çıkar</translation>
     </message>
     <message>
         <source>Accessing MusicBrainz...</source>
-        <translation type="unfinished"></translation>
+        <translation>Musicbrainz&apos;e Erişiliyor...</translation>
     </message>
     <message>
         <source>CD NOT FOUND</source>
-        <translation type="unfinished"></translation>
+        <translation>CD BULUNAMADI</translation>
     </message>
     <message>
         <source>This CD was not found on the MusicBrainz CD Database.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu CD MusicBrainz CD veritabanında bulunamadı.</translation>
     </message>
     <message>
         <source>SELECT CORRECT ALBUM</source>
-        <translation type="unfinished"></translation>
+        <translation>DOĞRU ALBÜMÜ SEÇ</translation>
     </message>
     <message>
         <source>More than one result was found on MusicBrainz for this CD. Select the correct CD for accurate track information.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu CD için Musicbrainz&apos;de birden fazla sonuç bulundu. Doğru şarkı bilgisi için doğru CD&apos;yi seçin.</translation>
     </message>
     <message>
         <source>Couldn&apos;t eject the disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk çıkarılamadı</translation>
     </message>
     <message>
         <source>Make sure no other applications are accessing the disc, and then try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Başka hiçbir uygulamanın diske erişmediğinden emin olun ve tekrar deneyin.</translation>
     </message>
 </context>
 <context>
     <name>MacBurnJob</name>
     <message>
         <source>Burn Complete</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanık Tamamlandı</translation>
     </message>
     <message>
         <source>Burn cancelled</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanık iptal edildi</translation>
     </message>
     <message>
         <source>Failed to burn: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazılamadı: %1</translation>
     </message>
     <message>
         <source>Erasing disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk siliniyor</translation>
     </message>
     <message>
         <source>Finalising disc</source>
-        <translation type="unfinished"></translation>
+        <translation>Disk sonlandırılıyor</translation>
     </message>
     <message>
         <source>Preparing to burn</source>
-        <translation type="unfinished"></translation>
+        <translation>Yanmaya hazırlanıyor</translation>
     </message>
     <message>
         <source>Burning track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazan şarkı %1</translation>
     </message>
     <message>
         <source>Burning</source>
-        <translation type="unfinished"></translation>
+        <translation>Yaniyor</translation>
     </message>
 </context>
 <context>
     <name>MacBurnJobPrivate</name>
     <message>
         <source>Burn Successful</source>
-        <translation type="unfinished"></translation>
+        <translation>Başarılı Yandı</translation>
     </message>
     <message>
         <source>Burned %1 to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 diske yazdırıldı</translation>
     </message>
     <message>
         <source>Burn Failure</source>
-        <translation type="unfinished"></translation>
+        <translation>Başarısız Yanık</translation>
     </message>
     <message>
         <source>Failed to burn %1 to disc</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 diske yazılamadı</translation>
     </message>
 </context>
 <context>
     <name>MacBurnJobWidget</name>
     <message>
         <source>BURN CD</source>
-        <translation type="unfinished"></translation>
+        <translation>CD OLUŞTURMAK</translation>
     </message>
 </context>
 <context>
     <name>MacBurnProvider</name>
     <message>
         <source>Insert a CD-R or CD-RW</source>
-        <translation type="unfinished"></translation>
+        <translation>Bir CD-R ya da CD-RW yerleştirin</translation>
     </message>
     <message>
         <source>Burn on macOS</source>
-        <translation type="unfinished"></translation>
+        <translation>MacOS&apos;ta yaz</translation>
     </message>
 </context>
 <context>
     <name>MusicBrainzReleaseModel</name>
     <message>
         <source>Released: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 çıktı</translation>
     </message>
     <message>
         <source>Barcode: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Barkod: %1</translation>
     </message>
     <message>
         <source>Country: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Ülke: %1</translation>
     </message>
 </context>
 <context>
     <name>TrackInfo</name>
     <message>
         <source>Track %1</source>
-        <translation type="unfinished"></translation>
+        <translation>1% Parça</translation>
     </message>
     <message>
         <source>Unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>Bilinmeyen</translation>
     </message>
 </context>
 </TS>
