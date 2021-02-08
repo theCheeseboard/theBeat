8ad80e701b283dcdbbf00616693c372b9f45f484
Victor Tran <vicr12345@gmail.com>
2021-02-08 14:47:15 +1100

    Change library names in AVFoundation plugin

--- ci/extra-steps-mac.sh
+++ ci/extra-steps-mac.sh
@@ -1,6 +1,6 @@
 # Modify the AVFoundation plugin
-install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundation.dylib 
-install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundation.dylib
+install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib 
+install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib build/application/theBeat.app/Contents/AppPlugins/libAvFoundationPlugin.dylib
 
 # Modify the Mac Integration plugin
 install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libMacIntegration.dylib 
