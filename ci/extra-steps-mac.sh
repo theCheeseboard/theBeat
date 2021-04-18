79e5b08db3ff5202c3e2fe4a785215781caadb86
Victor Tran <vicr12345@gmail.com>
2021-04-19 01:15:49 +1000

    Add extra steps to macOS CI

--- ci/extra-steps-mac.sh
+++ ci/extra-steps-mac.sh
@@ -5,3 +5,7 @@ install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libth
 # Modify the Mac Integration plugin
 install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libMacIntegration.dylib 
 install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib build/application/theBeat.app/Contents/AppPlugins/libMacIntegration.dylib
+
+# Modify the Internet Radio plugin
+install_name_tool -change libthe-libs.1.dylib @executable_path/../Libraries/libthe-libs.1.dylib build/application/theBeat.app/Contents/AppPlugins/libInternetRadioPlugin.dylib 
+install_name_tool -change libthebeat.1.dylib @executable_path/../Libraries/libthebeat.1.dylib build/application/theBeat.app/Contents/AppPlugins/libInternetRadioPlugin.dylib
