0eda90d86464b55e2cd471b1f7bf7bcc764b90b3
Victor Tran <vicr12345@gmail.com>
2021-07-25 20:08:14 +1000

    Update translations

---
+++ plugins/WinLibCDPlugin/translations/en_US.ts
@@ -0,0 +1,58 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CdChecker</name>
+    <message>
+        <location filename="../cdchecker.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="43"/>
+        <location filename="../cdchecker.cpp" line="44"/>
+        <location filename="../cdchecker.cpp" line="125"/>
+        <location filename="../cdchecker.cpp" line="126"/>
+        <source>CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="68"/>
+        <source>Enqueue All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="79"/>
+        <source>Play All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="90"/>
+        <source>Shuffle All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="114"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cdchecker.ui" line="125"/>
+        <source>Eject</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrackInfo</name>
+    <message>
+        <location filename="../trackinfo.cpp" line="39"/>
+        <source>Track %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../trackinfo.cpp" line="40"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
