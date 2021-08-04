12fa144aa0ef6cc66318767283d8b92598f3453e
Victor Tran <vicr12345@gmail.com>
2021-08-04 15:54:28 +1000

    Fix appx build script

--- appveyor/build.bat
+++ appveyor/build.bat
@@ -93,7 +93,7 @@ cd ..
 robocopy deploy deployAppx /mir
 cd deployAppx
 
-copy ..\dist\win-pack\* .
+robocopy ..\dist\win-pack\* . /mir
 makepri createconfig /cf priconfig.xml /dq en-US
 makepri new /pr . /cf priconfig.xml
 makeappx pack /d . /p theBeat.msix
