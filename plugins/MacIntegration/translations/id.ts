2f3c6a3a578d8d3c274c7cdc9332c8b6ff4303c2
Zumi <daxuya.zumi+code@protonmail.com>
2021-12-01 13:14:32 +0000

    Added translation using Weblate (Indonesian)

---
+++ plugins/MacIntegration/translations/id.ts
@@ -0,0 +1,195 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id">
+<context>
+    <name>CdChecker</name>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="43"/>
+        <location filename="../cd/cdchecker.cpp" line="62"/>
+        <location filename="../cd/cdchecker.cpp" line="128"/>
+        <location filename="../cd/cdchecker.cpp" line="129"/>
+        <location filename="../cd/cdchecker.cpp" line="130"/>
+        <source>CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="68"/>
+        <source>Enqueue All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="79"/>
+        <source>Play All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="89"/>
+        <source>Shuffle All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="112"/>
+        <source>Import</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="123"/>
+        <source>Eject</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="213"/>
+        <source>Accessing MusicBrainz...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="230"/>
+        <source>CD NOT FOUND</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="237"/>
+        <source>This CD was not found on the MusicBrainz CD Database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="257"/>
+        <source>SELECT CORRECT ALBUM</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker.ui" line="264"/>
+        <source>More than one result was found on MusicBrainz for this CD. Select the correct CD for accurate track information.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker-objc.mm" line="31"/>
+        <source>Couldn&apos;t eject the disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/cdchecker-objc.mm" line="32"/>
+        <source>Make sure no other applications are accessing the disc, and then try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MacBurnJob</name>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="79"/>
+        <source>Burn Complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="82"/>
+        <source>Burn cancelled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="84"/>
+        <source>Failed to burn: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="87"/>
+        <source>Erasing disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="91"/>
+        <source>Finalising disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="95"/>
+        <location filename="../burn/macburnjob.mm" line="97"/>
+        <source>Preparing to burn</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="99"/>
+        <source>Burning track %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="101"/>
+        <source>Burning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MacBurnJobPrivate</name>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="69"/>
+        <source>Burn Successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="69"/>
+        <source>Burned %1 to disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="72"/>
+        <source>Burn Failure</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnjob.mm" line="72"/>
+        <source>Failed to burn %1 to disc</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MacBurnJobWidget</name>
+    <message>
+        <location filename="../burn/macburnjobwidget.ui" line="26"/>
+        <source>BURN CD</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MacBurnProvider</name>
+    <message>
+        <location filename="../burn/macburnprovider-objc.mm" line="64"/>
+        <source>Insert a CD-R or CD-RW</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../burn/macburnprovider.cpp" line="11"/>
+        <source>Burn on macOS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MusicBrainzReleaseModel</name>
+    <message>
+        <location filename="../../PhononPlugin/musicbrainzreleasemodel.cpp" line="71"/>
+        <source>Released: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../PhononPlugin/musicbrainzreleasemodel.cpp" line="73"/>
+        <source>Barcode: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../PhononPlugin/musicbrainzreleasemodel.cpp" line="75"/>
+        <source>Country: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrackInfo</name>
+    <message>
+        <location filename="../cd/trackinfo.cpp" line="39"/>
+        <source>Track %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../cd/trackinfo.cpp" line="40"/>
+        <source>Unknown</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
