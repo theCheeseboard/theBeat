276c7a9a567588f726d73407d2b6f1f33f8087b5
Parlance <parlance@vicr123.com>
2022-11-04 23:34:31 +0000

    Update Translations - he_IL
    Co-Authored-By: martysh12 <martyshkon@gmail.com>

---
+++ plugins/CdrdaoPlugin/translations/he_IL.ts
@@ -0,0 +1,262 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>BurnJob</name>
+    <message>
+      <translation>מתכונן לצריבה</translation>
+      <location filename="../burnjob.cpp" line="71" />
+      <location filename="../burnjob.cpp" line="131" />
+      <source>Preparing to burn</source>
+    </message>
+    <message>
+      <translation>מכין שיר %1 לצריבה</translation>
+      <location filename="../burnjob.cpp" line="109" />
+      <source>Preparing Track %1 to be burned</source>
+    </message>
+    <message>
+      <translation>לא ניתן היה להמיר את השיר</translation>
+      <location filename="../burnjob.cpp" line="118" />
+      <source>Couldn't transcode track</source>
+    </message>
+    <message>
+      <translation>צורב שיר %1</translation>
+      <location filename="../burnjob.cpp" line="214" />
+      <source>Burning Track %1</source>
+    </message>
+    <message>
+      <translation>מסיים</translation>
+      <location filename="../burnjob.cpp" line="220" />
+      <source>Finalising CD</source>
+    </message>
+    <message>
+      <translation>בוטל</translation>
+      <location filename="../burnjob.cpp" line="258" />
+      <source>Cancelled</source>
+    </message>
+    <message>
+      <translation>לא ניתן לצרוב את השירים</translation>
+      <location filename="../burnjob.cpp" line="260" />
+      <source>Couldn't burn tracks</source>
+    </message>
+    <message>
+      <translation>הצריבה מוצלחת</translation>
+      <location filename="../burnjob.cpp" line="288" />
+      <location filename="../burnjob.cpp" line="296" />
+      <source>Burn Successful</source>
+    </message>
+    <message>
+      <translation>%1 נצרב לדיסק</translation>
+      <location filename="../burnjob.cpp" line="296" />
+      <source>Burned %1 to disc</source>
+    </message>
+    <message>
+      <translation>הצריבה נכשלה</translation>
+      <location filename="../burnjob.cpp" line="311" />
+      <source>Burn Failure</source>
+    </message>
+    <message>
+      <translation>לא ניתן לצרוב את %1 לדיסק</translation>
+      <location filename="../burnjob.cpp" line="311" />
+      <source>Failed to burn %1 to disc</source>
+    </message>
+  </context>
+  <context>
+    <name>BurnJobMp3</name>
+    <message>
+      <translation>מתכונן לצריבה</translation>
+      <location filename="../burnjobmp3.cpp" line="73" />
+      <location filename="../burnjobmp3.cpp" line="182" />
+      <source>Preparing to burn</source>
+    </message>
+    <message>
+      <translation>מכין %1 לצריבה</translation>
+      <location filename="../burnjobmp3.cpp" line="118" />
+      <source>Preparing %1 to be burned</source>
+    </message>
+    <message>
+      <translation>בוטל</translation>
+      <location filename="../burnjobmp3.cpp" line="127" />
+      <location filename="../burnjobmp3.cpp" line="166" />
+      <location filename="../burnjobmp3.cpp" line="268" />
+      <source>Cancelled</source>
+    </message>
+    <message>
+      <translation>לא ניתן היה להמיר את השיר</translation>
+      <location filename="../burnjobmp3.cpp" line="132" />
+      <source>Couldn't transcode track</source>
+    </message>
+    <message>
+      <translation>יוצר תמונת דיסק</translation>
+      <location filename="../burnjobmp3.cpp" line="156" />
+      <source>Generating Disc Image</source>
+    </message>
+    <message>
+      <translation>לא ניתן להכין את תמונת ה-ISO</translation>
+      <location filename="../burnjobmp3.cpp" line="168" />
+      <source>Couldn't prepare ISO image</source>
+    </message>
+    <message>
+      <translation>צורב דיסק</translation>
+      <location filename="../burnjobmp3.cpp" line="217" />
+      <source>Burning Disc</source>
+    </message>
+    <message>
+      <translation>מסיים</translation>
+      <location filename="../burnjobmp3.cpp" line="223" />
+      <source>Finalising CD</source>
+    </message>
+    <message>
+      <translation>צורב %1
+%2 מ-%3</translation>
+      <location filename="../burnjobmp3.cpp" line="244" />
+      <source>Burning %1
+%2 of %3</source>
+    </message>
+    <message>
+      <translation>לא ניתן לצרוב את השירים</translation>
+      <location filename="../burnjobmp3.cpp" line="270" />
+      <source>Couldn't burn tracks</source>
+    </message>
+    <message>
+      <translation>הצריבה מוצלחת</translation>
+      <location filename="../burnjobmp3.cpp" line="298" />
+      <location filename="../burnjobmp3.cpp" line="306" />
+      <source>Burn Successful</source>
+    </message>
+    <message>
+      <translation>%1 נצרב לדיסק</translation>
+      <location filename="../burnjobmp3.cpp" line="306" />
+      <source>Burned %1 to disc</source>
+    </message>
+    <message>
+      <translation>הצריבה נכשלה</translation>
+      <location filename="../burnjobmp3.cpp" line="321" />
+      <source>Burn Failure</source>
+    </message>
+    <message>
+      <translation>לא ניתן לצרוב את %1 לדיסק</translation>
+      <location filename="../burnjobmp3.cpp" line="321" />
+      <source>Failed to burn %1 to disc</source>
+    </message>
+  </context>
+  <context>
+    <name>BurnJobWidget</name>
+    <message>
+      <translation>צרוב CD</translation>
+      <location filename="../burnjobwidget.ui" line="26" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnjobwidget.h" line="100" />
+      <source>BURN CD</source>
+    </message>
+  </context>
+  <context>
+    <name>BurnPopover</name>
+    <message>
+      <translation>Audio CD</translation>
+      <location filename="../burnpopover.ui" line="67" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="229" />
+      <source>Audio CD</source>
+    </message>
+    <message>
+      <translation>סוג דיסק</translation>
+      <location filename="../burnpopover.ui" line="80" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="230" />
+      <source>Disc Type</source>
+    </message>
+    <message>
+      <translation>אפשרויות צריבה</translation>
+      <location filename="../burnpopover.ui" line="92" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="231" />
+      <source>BURN OPTIONS</source>
+    </message>
+    <message>
+      <translation>צרוב</translation>
+      <location filename="../burnpopover.ui" line="99" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="232" />
+      <source>Burn</source>
+    </message>
+    <message>
+      <translation>שם אלבום</translation>
+      <location filename="../burnpopover.ui" line="110" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="233" />
+      <source>Album Name</source>
+    </message>
+    <message>
+      <translation>שימו לב</translation>
+      <location filename="../burnpopover.ui" line="134" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="234" />
+      <source>HEADS UP</source>
+    </message>
+    <message>
+      <translation>MP3 CD</translation>
+      <location filename="../burnpopover.ui" line="154" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="235" />
+      <source>MP3 CD</source>
+    </message>
+    <message>
+      <translation>צרוב דיסק</translation>
+      <location filename="../burnpopover.ui" line="196" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="236" />
+      <source>Burn Disc</source>
+    </message>
+    <message>
+      <translation>דיסק אינו ריק</translation>
+      <location filename="../burnpopover.ui" line="211" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="237" />
+      <source>DISC NOT BLANK</source>
+    </message>
+    <message>
+      <translation>הדיסק בכונן הוא אינו ריק. כדי לצרוב שירים אלה לדיסק, עליך למחוק את הדיסק.
+ברגע שהדיסק בכונן נמחק, כל המידע עליו נעלם לתמיד.</translation>
+      <location filename="../burnpopover.ui" line="218" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="238" />
+      <source>The disc in the drive is not blank. To burn these tracks to the disc, the disc will need to be erased.
+
+Once the disc in the drive is erased, all the data on it will be gone forever.</source>
+    </message>
+    <message>
+      <translation>מחק וצרוב</translation>
+      <location filename="../burnpopover.ui" line="230" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/CdrdaoPlugin/plugin-cdrdao_autogen/include/ui_burnpopover.h" line="241" />
+      <source>Erase and Burn</source>
+    </message>
+    <message>
+      <translation>צרוב את %1</translation>
+      <location filename="../burnpopover.cpp" line="96" />
+      <location filename="../burnpopover.cpp" line="97" />
+      <source>Burn %1</source>
+    </message>
+    <message>
+      <translation>הכנס CD-R או CD-RW לכונן.</translation>
+      <location filename="../burnpopover.cpp" line="105" />
+      <source>Insert a CD-R or a CD-RW into the drive.</source>
+    </message>
+    <message>
+      <translation>ה-CD-R בכונן כבר נכתב.</translation>
+      <location filename="../burnpopover.cpp" line="109" />
+      <source>The CD-R in the drive has already been written.</source>
+    </message>
+    <message>
+      <translation>רשימת ההשמעה זו גדולה מדי כדי להתאים ב-CD.</translation>
+      <location filename="../burnpopover.cpp" line="139" />
+      <source>This playlist is too long to fit on the CD.</source>
+    </message>
+    <message>
+      <translation>ה-CD בכונן ניתן לכתיבה מחדש, כך שייתכן שה-CD הצרוב לא יעבוד על נגני CD ישנים.</translation>
+      <location filename="../burnpopover.cpp" line="143" />
+      <source>The CD in the drive is rewritable, so the burned CD may not work on older CD players.</source>
+    </message>
+  </context>
+  <context>
+    <name>FullBurnJob</name>
+    <message>
+      <translation>מחיקת הדיסק לפני הצריבה נכשלה</translation>
+      <location filename="../fullburnjob.cpp" line="157" />
+      <source>Failed to erase the disc before burning</source>
+    </message>
+    <message>
+      <translation>מוחק דיסק</translation>
+      <location filename="../fullburnjob.cpp" line="158" />
+      <source>Erasing disc</source>
+    </message>
+  </context>
+</TS>
---
+++ plugins/InternetRadioPlugin/translations/he_IL.ts
@@ -0,0 +1,53 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>FavouriteStationsWidget</name>
+    <message>
+      <translation>רדיו אינטרנט</translation>
+      <location filename="../favouritestationswidget.ui" line="57" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_favouritestationswidget.h" line="105" />
+      <source>Internet Radio</source>
+    </message>
+    <message>
+      <translation>תהנה להאזין לתחנות הרדיו האהובות עליך</translation>
+      <location filename="../favouritestationswidget.ui" line="80" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_favouritestationswidget.h" line="107" />
+      <source>Jam out to your favourite radio stations and radio stations all around the world</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../favouritestationswidget.ui" line="93" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_favouritestationswidget.h" line="108" />
+      <source>Go Searching</source>
+    </message>
+  </context>
+  <context>
+    <name>RadioPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../radiopane.cpp" line="38" />
+      <source>Internet Radio</source>
+    </message>
+  </context>
+  <context>
+    <name>StationSearchWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../stationsearchwidget.ui" line="35" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_stationsearchwidget.h" line="161" />
+      <source>Internet Radio</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../stationsearchwidget.ui" line="45" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_stationsearchwidget.h" line="162" />
+      <source>Search...</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../stationsearchwidget.ui" line="111" />
+      <location filename="../../../../build-theBeat-Chroot_6-Debug/plugins/InternetRadioPlugin/plugin-internetradio_autogen/include/ui_stationsearchwidget.h" line="163" />
+      <source>TOP VOTED</source>
+    </message>
+  </context>
+</TS>
