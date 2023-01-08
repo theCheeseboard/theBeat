49573308e14c60257ca99791eca5b862eebfc572
Parlance <parlance@vicr123.com>
2023-01-08 21:22:59 +0000

    Update Translations - cs_CZ

---
+++ plugins/InternetRadioPlugin/translations/cs_CZ.ts
@@ -0,0 +1,47 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="cs_CZ">
+  <context>
+    <name>FavouriteStationsWidget</name>
+    <message>
+      <translation>Internetové rádio</translation>
+      <location filename="../favouritestationswidget.ui" line="57" />
+      <source>Internet Radio</source>
+    </message>
+    <message>
+      <translation>Poslouchejte všechny vaše oblíbené rádiové stanice a rádiové stanice kolem světa</translation>
+      <location filename="../favouritestationswidget.ui" line="80" />
+      <source>Jam out to your favourite radio stations and radio stations all around the world</source>
+    </message>
+    <message>
+      <translation>Vyhledat</translation>
+      <location filename="../favouritestationswidget.ui" line="93" />
+      <source>Go Searching</source>
+    </message>
+  </context>
+  <context>
+    <name>RadioPane</name>
+    <message>
+      <translation>Internetové rádio</translation>
+      <location filename="../radiopane.cpp" line="38" />
+      <source>Internet Radio</source>
+    </message>
+  </context>
+  <context>
+    <name>StationSearchWidget</name>
+    <message>
+      <translation>Internetové rádio</translation>
+      <location filename="../stationsearchwidget.ui" line="35" />
+      <source>Internet Radio</source>
+    </message>
+    <message>
+      <translation>Hledat...</translation>
+      <location filename="../stationsearchwidget.ui" line="45" />
+      <source>Search...</source>
+    </message>
+    <message>
+      <translation>NEJLÉPE HLASOVÁNO</translation>
+      <location filename="../stationsearchwidget.ui" line="111" />
+      <source>TOP VOTED</source>
+    </message>
+  </context>
+</TS>
