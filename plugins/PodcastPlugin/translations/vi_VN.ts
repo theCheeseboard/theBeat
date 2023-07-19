47c67dd600dfd31588b46d1bc198cafaa63a4b46
Parlance <parlance@vicr123.com>
2023-07-19 08:06:59 +0000

    Update Translations - vi_VN
    Co-Authored-By: Victor Tran <vicr12345@gmail.com>

---
+++ plugins/PodcastPlugin/translations/vi_VN.ts
@@ -0,0 +1,251 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1" language="vi_VN">
+  <context>
+    <name>Podcast</name>
+    <message>
+      <translation>Cho %1</translation>
+      <location filename="../podcast.cpp" line="185" />
+      <source>For %1</source>
+    </message>
+    <message>
+      <translation>Hủy đăng ký</translation>
+      <location filename="../podcast.cpp" line="189" />
+      <location filename="../podcast.cpp" line="191" />
+      <source>Unsubscribe</source>
+    </message>
+    <message>
+      <translation>Có chắc chắn không?</translation>
+      <location filename="../podcast.cpp" line="190" />
+      <source>Are you sure?</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastItem</name>
+    <message>
+      <translation>Tải xuống tập Podcast</translation>
+      <location filename="../podcastitem.cpp" line="152" />
+      <source>Download Podcast Episode</source>
+    </message>
+    <message>
+      <translation>Đang tải xuống %1</translation>
+      <location filename="../podcastitem.cpp" line="153" />
+      <source>Downloading %1</source>
+    </message>
+    <message>
+      <translation>Không thể tải xuống %1</translation>
+      <location filename="../podcastitem.cpp" line="183" />
+      <source>Failed to download %1</source>
+    </message>
+    <message>
+      <translation>Đã tải xuống %1</translation>
+      <location filename="../podcastitem.cpp" line="198" />
+      <source>Downloaded %1</source>
+    </message>
+    <message>
+      <translation>Cho %1</translation>
+      <location filename="../podcastitem.cpp" line="220" />
+      <source>For %1</source>
+    </message>
+    <message>
+      <translation>Xóa bản tải xuống</translation>
+      <location filename="../podcastitem.cpp" line="224" />
+      <source>Remove Download</source>
+    </message>
+    <message>
+      <translation>Tải xuống</translation>
+      <location filename="../podcastitem.cpp" line="228" />
+      <source>Download</source>
+    </message>
+    <message>
+      <translation>Đánh dấu là chưa nghe</translation>
+      <location filename="../podcastitem.cpp" line="235" />
+      <source>Mark as unplayed</source>
+    </message>
+    <message>
+      <translation>Đánh dấu là đã nghe</translation>
+      <location filename="../podcastitem.cpp" line="239" />
+      <source>Mark as complete</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastItemDelegate</name>
+    <message>
+      <translation>Mới</translation>
+      <location filename="../models/podcastmodel.cpp" line="94" />
+      <source>New</source>
+    </message>
+    <message>
+      <translation>Đã nghe</translation>
+      <location filename="../models/podcastmodel.cpp" line="90" />
+      <source>Listened</source>
+    </message>
+    <message>
+      <translation>%1 còn lại</translation>
+      <location filename="../models/podcastmodel.cpp" line="96" />
+      <source>%1 remaining</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastItemWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/podcastitemwidget.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation>Bài trong thư viện</translation>
+      <location filename="../widgets/podcastitemwidget.ui" line="109" />
+      <source>Tracks in Library</source>
+    </message>
+    <message>
+      <translation>Phát</translation>
+      <location filename="../widgets/podcastitemwidget.ui" line="158" />
+      <source>Play</source>
+    </message>
+    <message>
+      <translation>Đang tải xuống...</translation>
+      <location filename="../widgets/podcastitemwidget.cpp" line="58" />
+      <source>Download in progress...</source>
+    </message>
+    <message>
+      <translation>Xóa bản tải xuống</translation>
+      <location filename="../widgets/podcastitemwidget.cpp" line="61" />
+      <source>Remove Download</source>
+    </message>
+    <message>
+      <translation>Tải xuống</translation>
+      <location filename="../widgets/podcastitemwidget.cpp" line="63" />
+      <source>Download</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastListingWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/podcastlistingwidget.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation>Đật bài chưa nghe vào hàng phát</translation>
+      <location filename="../widgets/podcastlistingwidget.ui" line="158" />
+      <source>Enqueue All Incomplete</source>
+    </message>
+    <message>
+      <translation>Phát bài chưa nghe</translation>
+      <location filename="../widgets/podcastlistingwidget.ui" line="169" />
+      <source>Play All Incomplete</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform>%n tập</numerusform>
+      </translation>
+      <location filename="../widgets/podcastlistingwidget.cpp" line="61" />
+      <source>%n episodes</source>
+    </message>
+    <message>
+      <translation>Bạn đã cập nhật</translation>
+      <location filename="../widgets/podcastlistingwidget.cpp" line="91" />
+      <location filename="../widgets/podcastlistingwidget.cpp" line="108" />
+      <source>Up to date</source>
+    </message>
+    <message>
+      <translation>Bạn đã nghe các tập trong podcast này rồi.</translation>
+      <location filename="../widgets/podcastlistingwidget.cpp" line="92" />
+      <location filename="../widgets/podcastlistingwidget.cpp" line="109" />
+      <source>You've listened to every episode in this podcast.</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastManager</name>
+    <message>
+      <translation>Đang cập nhật podcast</translation>
+      <location filename="../podcastmanager.cpp" line="72" />
+      <source>Updating Podcasts</source>
+    </message>
+    <message>
+      <translation>Đang cập nhật %1</translation>
+      <location filename="../podcastmanager.cpp" line="75" />
+      <source>Updating %1</source>
+    </message>
+    <message>
+      <translation>Đã cập nhật podcast</translation>
+      <location filename="../podcastmanager.cpp" line="85" />
+      <source>Updated Podcasts</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/podcastpane.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation>Podcast</translation>
+      <location filename="../widgets/podcastpane.cpp" line="22" />
+      <source>Podcasts</source>
+    </message>
+  </context>
+  <context>
+    <name>PodcastSettingsPane</name>
+    <message>
+      <translation></translation>
+      <location filename="../settingspanes/podcastsettingspane.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation>Tự động tải xuống các tập podcast</translation>
+      <location filename="../settingspanes/podcastsettingspane.ui" line="20" />
+      <source>Download new podcast episodes automatically</source>
+    </message>
+    <message>
+      <translation>Podcast</translation>
+      <location filename="../settingspanes/podcastsettingspane.cpp" line="25" />
+      <source>Podcasts</source>
+    </message>
+  </context>
+  <context>
+    <name>SubscribedPodcastsWidget</name>
+    <message>
+      <translation></translation>
+      <location filename="../widgets/subscribedpodcastswidget.ui" line="14" />
+      <source>Form</source>
+    </message>
+    <message>
+      <translation>Podcast</translation>
+      <location filename="../widgets/subscribedpodcastswidget.ui" line="66" />
+      <source>Podcasts</source>
+    </message>
+    <message>
+      <translation>Cập nhật tất cả</translation>
+      <location filename="../widgets/subscribedpodcastswidget.ui" line="78" />
+      <source>Update All</source>
+    </message>
+    <message>
+      <translation>Đăng ký</translation>
+      <location filename="../widgets/subscribedpodcastswidget.ui" line="89" />
+      <source>Add Subscription</source>
+    </message>
+    <message>
+      <translation>Đăng ký podcast mới</translation>
+      <location filename="../widgets/subscribedpodcastswidget.cpp" line="47" />
+      <location filename="../widgets/subscribedpodcastswidget.cpp" line="55" />
+      <source>Subscribe to new podcast</source>
+    </message>
+    <message>
+      <translation>Địa chỉ nguồn cấp là gì?</translation>
+      <location filename="../widgets/subscribedpodcastswidget.cpp" line="55" />
+      <source>What's the address of the feed?</source>
+    </message>
+    <message>
+      <translation>URL không hợp lệ</translation>
+      <location filename="../widgets/subscribedpodcastswidget.cpp" line="60" />
+      <source>Invalid URL</source>
+    </message>
+    <message>
+      <translation>Rất tiếc, URL đó không phải URL nguồn cấp hợp lệ</translation>
+      <location filename="../widgets/subscribedpodcastswidget.cpp" line="61" />
+      <source>Sorry, that URL is not a valid feed URL</source>
+    </message>
+  </context>
+</TS>
