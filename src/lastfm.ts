27ac7185369e7119bc36c4669f888a2d23c7b857
Victor Tran <vicr12345@gmail.com>
2023-08-30 20:50:15 +1000

    Add POST method handling in LastFmRouter
    Added support of POST method handling in LastFmRouter. Now it can switch between 'track.scrobble' and other methods based on the provided JSON. Included a default bad method response for unsupported queries. Additionally, handled response from LastFM by setting 'X-theBeat-Api-Key' and 'Content-Type' in response headers.

--- src/lastfm.ts
+++ src/lastfm.ts
@@ -1,6 +1,11 @@
 import { Router } from 'itty-router'
 import { Env } from './worker';
 
+interface LastFmPostQuery {
+	method: "track.scrobble"
+	[key: string]: any
+}
+
 interface LastFmGetQuery {
 	method: "auth.getToken" | "auth.getSession";
 }
@@ -37,7 +42,31 @@ LastFmRouter.get("/", async (request, env: Env) => {
 	});
 })
 
-async function callLastFm(args: any, env: Env) {
+LastFmRouter.post("/", async (request, env: Env) => {
+	const json = await request.json() as LastFmPostQuery;
+
+	switch (json.method) {
+		case 'track.scrobble':
+			break;
+		default:
+			return new Response("Bad Method", {
+				status: 400
+			});
+	}
+
+	const response = await postLastFm(json, env);
+
+	const body = await response.arrayBuffer();
+	return new Response(body, {
+		status: response.status,
+		headers: {
+			"X-theBeat-Api-Key": env.LASTFM_API_KEY,
+			"Content-Type": "application/json"
+		}
+	});
+})
+
+async function transformArgs(args: any, env: Env) {
 	const modifiedArgs = {
 		...args,
 		"api_key": env.LASTFM_API_KEY
@@ -49,20 +78,31 @@ async function callLastFm(args: any, env: Env) {
 	const hash = await crypto.subtle.digest("MD5", data);
 	const hex =  Array.prototype.map.call(new Uint8Array(hash), x => ('00' + x.toString(16)).slice(-2)).join('');
 
-	const argsWithSig = {
+	return {
 		...modifiedArgs,
 		"format": "json",
 		"api_sig": hex
 	};
+}
 
-	console.log(params);
-
-	const url = `https://ws.audioscrobbler.com/2.0/?${new URLSearchParams(argsWithSig).toString()}`;
-	console.log(url);
-
+async function callLastFm(args: any, env: Env) {
+	const transformedArgs = await transformArgs(args, env);
+	const url = `https://ws.audioscrobbler.com/2.0/?${new URLSearchParams(transformedArgs).toString()}`;
 	return await fetch(url, {
 		headers: {
 			"User-Agent": "theBeatProxy/1.0 (vicr12345@gmail.com)"
 		}
 	})
+}
+
+async function postLastFm(args: any, env: Env) {
+	const transformedArgs = await transformArgs(args, env);
+	const url = "https://ws.audioscrobbler.com/2.0/";
+	return await fetch(url, {
+		headers: {
+			"User-Agent": "theBeatProxy/1.0 (vicr12345@gmail.com)"
+		},
+		method: "POST",
+		body: new URLSearchParams(transformedArgs).toString()
+	});
 }
