f4d22415e203105ef865bc0532101734fdd080f2
John Tur <john-tur@outlook.com>
2024-12-02 09:22:03 -0500

    Add libcdio vcpkg port

---
+++ plugins/ParanoiaPlugin/overlay-ports/libcdio/portfile.cmake
@@ -0,0 +1,38 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL https://git.savannah.gnu.org/git/libcdio.git
+    REF 9c7a2779846da161279bc1501e83c849cf89a594
+    PATCHES
+        remove-off_t-override.patch
+        remove-unistd-include.patch
+        remove-printf-define.patch
+        remove-pretty-function.patch
+        remove-docs-tests-examples.patch
+        require-stdbool.patch
+        rename-in-macro.patch
+        fix-globals-linkage.patch
+        fix-varargs-macro.patch
+        disable-mmc-tool.patch
+)
+
+vcpkg_configure_make(
+    SOURCE_PATH ${SOURCE_PATH}
+    AUTOCONFIG
+    ADDITIONAL_MSYS_PACKAGES texinfo
+    OPTIONS
+        ${OPTIONS}
+        --without-cd-drive
+        --without-cd-info
+        --without-cdda-player
+        --without-cd-read
+        --without-iso-info
+        --without-iso-read
+        --without-mmc-tool
+)
+
+vcpkg_install_make()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
