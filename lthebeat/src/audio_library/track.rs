d3025021af38752813a7b5f2cdd8c73513e758ec
Victor Tran <vicr12345@gmail.com>
2025-09-02 23:39:03 +1000

    Enqueue track by clicking

--- lthebeat/src/audio_library/track.rs
+++ lthebeat/src/audio_library/track.rs
@@ -1,7 +1,12 @@
 use crate::audio_library::database_query::DatabaseRecord;
+use crate::play_queue::PlayQueue;
+use crate::play_queue::media_item::MediaItem;
 use cntp_i18n::tr;
 use contemporary::components::spinner::spinner;
-use gpui::{Context, Element, IntoElement, ParentElement, Render, Window, div};
+use gpui::{
+    Context, Element, ElementId, InteractiveElement, IntoElement, ParentElement, Render,
+    StatefulInteractiveElement, Window, div,
+};
 use sqlx::sqlite::SqliteRow;
 use sqlx::{Error, Row};
 use url::Url;
@@ -9,6 +14,7 @@ use url::Url;
 #[derive(Default)]
 pub enum Track {
     Ok {
+        id: usize,
         url: Url,
         name: String,
     },
@@ -21,9 +27,22 @@ pub enum Track {
 impl Render for Track {
     fn render(&mut self, window: &mut Window, cx: &mut Context<Self>) -> impl IntoElement {
         match self {
-            Track::Ok { name, .. } => div().child(name.to_string()),
-            Track::Loading => div().child(spinner()),
-            Track::Error => div().child(tr!("LIBRARY_TRACKS_ERROR", "Error loading tracks")),
+            Track::Ok { name, id, url } => {
+                let url_clone = url.clone();
+                div()
+                    .id(ElementId::from(*id))
+                    .child(name.to_string())
+                    .on_click(cx.listener(move |_, _, _, cx| {
+                        let item = MediaItem::new(url_clone.clone(), cx);
+                        let play_queue = cx.global_mut::<PlayQueue>();
+                        play_queue.add_item(item);
+                    }))
+                    .into_any_element()
+            }
+            Track::Loading => div().child(spinner()).into_any_element(),
+            Track::Error => div()
+                .child(tr!("LIBRARY_TRACKS_ERROR", "Error loading tracks"))
+                .into_any_element(),
         }
     }
 }
@@ -32,6 +51,7 @@ impl DatabaseRecord for Track {
     fn read_from_row(&mut self, row: Result<SqliteRow, Error>) {
         if let Ok(row) = row {
             *self = Track::Ok {
+                id: row.get::<u32, _>("id") as usize,
                 url: Url::parse(row.get::<String, _>("url").as_str()).unwrap(),
                 name: row.get::<String, _>("name"),
             }
