58612c1e7cd4da73152565c2c7f6c71e50371329
Victor Tran <vicr12345@gmail.com>
2025-09-07 22:08:36 +1000

    Configure Windows subsystem

--- thebeat/src/main.rs
+++ thebeat/src/main.rs
@@ -1,3 +1,6 @@
+// On Windows do NOT show a console window when opening the app
+#![cfg_attr(all(not(test), target_os = "windows"), windows_subsystem = "windows")]
+
 mod actions;
 mod main_surface;
 mod main_window;
